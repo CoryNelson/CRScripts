e70d1822eacfe84d58169f12242fa0bf3185042f
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-22 01:28:36 +0100

    First Commit

--- src/com/crscripts/domtower/boss/AbstractBoss.java
+++ src/com/crscripts/domtower/boss/AbstractBoss.java
@@ -1,15 +1,17 @@
 package com.crscripts.domtower.boss;
 
-import com.crscripts.domtower.mode.climber.handicap.Handicaps;
 import com.crscripts.domtower.util.Consumables;
 import com.crscripts.domtower.util.Methods;
 import com.crscripts.domtower.util.prayer.Pray;
 import com.crscripts.domtower.util.prayer.Protect;
-import org.powerbot.game.api.methods.Walking;
 import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.widget.Camera;
 import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.wrappers.interactive.NPC;
+
+import static com.crscripts.domtower.mode.climber.handicap.Handicaps.NO_FOOD;
+import static com.crscripts.domtower.mode.climber.handicap.Handicaps.NO_PRAYERS;
 
 /**
  * User: Cory
@@ -28,25 +30,29 @@ public abstract class AbstractBoss extends Methods {
 		this.bossIds = bossIds;
 	}
 
+	public AbstractBoss(int... bossIds) {
+		this(Protect.NONE, bossIds);
+	}
+
 	protected void run() {
-		if(validate()) {
+		if (validate()) {
 			if (attack || Players.getLocal().getInteracting() == null) {
 				attack = false;
 				if (!boss.isOnScreen())
-					Walking.walk(boss);
-				if (boss != null && boss.interact("Attack")) {
+					Camera.turnTo(boss);
+				if (boss.interact("Attack")) {
 					int time = 0;
-					while ((boss != null && !boss.isInCombat()) && time <= 5000) {
+					while (!boss.isInCombat() && time <= 5000) {
 						Time.sleep(50);
 						time += 50;
 					}
 				}
 			}
-			if(protect != null && protect != Protect.NONE) {
+			if (protect != null && protect != Protect.NONE) {
 				if (prayerIsLow() && consume(2, Consumables.PRAY_POT))
-						attack = true;
+					attack = true;
 				if (hpIsLow() && consume(2, Consumables.FOOD))
-						attack = true;
+					attack = true;
 				if (!Pray.prayerActive(protect.getId()))
 					Pray.toggle(protect.getId());
 			}
@@ -60,11 +66,7 @@ public abstract class AbstractBoss extends Methods {
 	}
 
 	protected boolean resetProgress() {
-		if (Handicaps.NO_FOOD.isActive())
-			return true;
-		if (Handicaps.NO_PRAYERS.isActive())
-			return true;
-		return false;
+		return (NO_FOOD.isActive() || NO_PRAYERS.isActive());
 	}
 
 	protected abstract int[] getNeededItems();
