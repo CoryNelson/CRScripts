9702ea6d47e91d3d5aa9ed7d8ed7f062bb78e122
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-22 01:03:05 +0100

    First Commit

---
+++ src/com/crscripts/domtower/boss/bosses/TarnRazorlor.java
@@ -0,0 +1,29 @@
+package com.crscripts.domtower.boss.bosses;
+
+import com.crscripts.domtower.boss.AbstractBoss;
+import com.crscripts.domtower.util.prayer.Pray;
+import com.crscripts.domtower.util.prayer.Protect;
+
+/**
+ * User: Cory
+ * Date: 22/08/12
+ * Time: 00:58
+ */
+public class TarnRazorlor extends AbstractBoss {
+
+	public TarnRazorlor() {
+		super(Protect.MELEE, new int[]{-1});
+	}
+
+	protected void run() {
+		super.run();
+		if (boss != null && boss.validate()) {
+			if (!Pray.prayerActive(19))
+				Pray.toggle(19);
+		}
+	}
+
+	protected int[] getNeededItems() {
+		return null;
+	}
+}
