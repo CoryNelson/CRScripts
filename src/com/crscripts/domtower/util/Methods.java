27c981160e60d9c6d3d1c73415c2cf8874bab7d0
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-22 01:46:14 +0100

    First Commit

--- src/com/crscripts/domtower/util/Methods.java
+++ src/com/crscripts/domtower/util/Methods.java
@@ -3,13 +3,15 @@ package com.crscripts.domtower.util;
 import org.powerbot.game.api.methods.Calculations;
 import org.powerbot.game.api.methods.Walking;
 import org.powerbot.game.api.methods.Widgets;
-import org.powerbot.game.api.methods.input.Mouse;
+import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.SceneEntities;
 import org.powerbot.game.api.methods.tab.Inventory;
 import org.powerbot.game.api.methods.tab.Skills;
 import org.powerbot.game.api.methods.widget.Camera;
 import org.powerbot.game.api.util.Time;
+import org.powerbot.game.api.wrappers.Locatable;
+import org.powerbot.game.api.wrappers.interactive.NPC;
 import org.powerbot.game.api.wrappers.node.Item;
 import org.powerbot.game.api.wrappers.node.SceneObject;
 
@@ -38,25 +40,42 @@ public class Methods {
 
 	protected double getPercent(int lvl, int skill, int boost) {
 		double realLvl = Skills.getRealLevel(Skills.PRAYER) * boost;
-		return ((double)lvl / realLvl) * 100;
+		return ((double) lvl / realLvl) * 100;
 	}
 
-	protected boolean walkToAndClick(String action, int... id) {
+	protected boolean interactWithObject(String action, int... id) {
 		SceneObject o = SceneEntities.getNearest(id);
-		if (Calculations.distanceTo(o) > 5 && !Players.getLocal().isMoving())
-			Walking.walk(o);
-		else {
-			if (o != null && o.isOnScreen()) {
-				Mouse.move(o.getCentralPoint(), 30, 50);
-				return (o.interact(action));
-			} else {
-				Camera.turnTo(o);
-			}
-		}
+		if (canInteract(o, 5))
+			return (o.interact(action));
 		return false;
 	}
 
+	protected boolean interactWithNPC(String action, int... id) {
+		NPC n = NPCs.getNearest(id);
+		if (canInteract(n, 5))
+			return (n.interact(action));
+		return false;
+	}
+
+	protected boolean canInteract(Locatable loc, int dist) {
+		if (Calculations.distanceTo(loc) > dist && !Players.getLocal().isMoving()) {
+			Walking.walk(loc);
+			return false;
+		}
+		if (!loc.getLocation().isOnScreen()) {
+			Camera.turnTo(loc);
+			return false;
+		}
+		return true;
+	}
+
 	protected boolean consume(int amt, int... id) {
+		for (int i = 0; i < amt; i++)
+			return consume(id);
+		return false;
+	}
+
+	protected boolean consume(int... id) {
 		for (int i : id) {
 			for (Item s : Inventory.getItems()) {
 				if (s.getId() == i) {
