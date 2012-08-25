6d721f81d80363feefcadb6b371b6760b4225646
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-25 22:08:33 +0100

    First Commit

--- src/com/crscripts/CRPker/tasks/EnterWild.java
+++ src/com/crscripts/CRPker/tasks/EnterWild.java
@@ -2,8 +2,12 @@ package com.crscripts.CRPker.tasks;
 
 import com.crscripts.CRPker.util.Util;
 import org.powerbot.concurrent.strategy.Strategy;
+import org.powerbot.game.api.methods.Calculations;
+import org.powerbot.game.api.methods.Walking;
+import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.SceneEntities;
 import org.powerbot.game.api.util.Time;
+import org.powerbot.game.api.wrappers.Tile;
 
 /**
  * User: Cory
@@ -12,15 +16,22 @@ import org.powerbot.game.api.util.Time;
  */
 public class EnterWild extends Strategy implements Runnable {
 	public boolean validate() {
+		if ((Calculations.distanceTo(2815, 5511) < 2) && !Players.getLocal().isMoving())
+			return true;
 		if (Util.isWieldingEquipment() && Util.inventoryHasItems())
 			return (SceneEntities.getNearest(38698) != null);
 		return false;
 	}
 
 	public void run() {
-		if (Util.interactWithObject("Enter", 38698)) {
+		System.out.println("o: " + (Calculations.distanceTo(2815, 5511)));
+		if ((Calculations.distanceTo(2815, 5511) < 2)) {
+			Time.sleep(400, 500);
+			Tile tile = Players.getLocal().getLocation();
+			Walking.walk(new Tile(tile.getX(), tile.getY() + 4, 0).randomize(2, 3));
+		} else if (Util.interactWithObject("Enter", 38698)) {
 			int time = 0;
-			while ((SceneEntities.getNearest(38700) == null) && time <= 4000) {
+			while ((Calculations.distanceTo(2815, 5511) > 250) && time <= 4000) {
 				time += 50;
 				Time.sleep(50);
 			}
