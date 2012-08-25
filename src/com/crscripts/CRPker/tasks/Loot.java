1fcffac0293333230d50f012733ebe7314daa325
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-25 19:38:57 +0100

    First Commit

---
+++ src/com/crscripts/CRPker/tasks/Loot.java
@@ -0,0 +1,18 @@
+package com.crscripts.CRPker.tasks;
+
+import org.powerbot.concurrent.strategy.Strategy;
+
+/**
+ * User: Cory
+ * Date: 25/08/12
+ * Time: 19:38
+ */
+public class Loot extends Strategy implements Runnable {
+	public boolean validate() {
+		return false;
+	}
+
+	public void run() {
+
+	}
+}
