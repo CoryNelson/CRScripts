060d40dc787205220eadfaca557144b46daf4957
CoryNelson <spudr0x@hotmail.co.uk>
2012-08-25 19:36:56 +0100

    First Commit

---
+++ src/com/crscripts/CRPker/tasks/Consumables.java
@@ -0,0 +1,18 @@
+package com.crscripts.CRPker.tasks;
+
+import org.powerbot.concurrent.strategy.Strategy;
+
+/**
+ * User: Cory
+ * Date: 25/08/12
+ * Time: 19:35
+ */
+public class Consumables extends Strategy implements Runnable {
+	public boolean validate() {
+		return false;
+	}
+
+	public void run() {
+
+	}
+}
